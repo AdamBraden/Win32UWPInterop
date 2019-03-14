27b65561af55f731308cc474a51961b45f1fd849
Adam Braden <adambr@microsoft.com>
2019-03-14 13:43:39 -0700

    Initialize

---
+++ NativeApp/MainPage.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+// #include "ManagedWinMD.h"
+// #include "Generated Files/winrt/ManagedWinMD.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+
+namespace winrt::NativeApp::implementation
+{
+    MainPage::MainPage()
+    {
+		//Initialize Managed CLR
+		// HRESULT hr = WINRT_RoInitialize(NULL);
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+	/*	ManagedWinMD::Class1 c1;
+		int x = c1.ReturnPlusOne(5);
+    */    
+	   myButton().Content(box_value(L"Clicked"));
+    }
+	void MainPage::MyOtherButton_Click(IInspectable const& sender, RoutedEventArgs const& e)
+	{
+		//UnInitialize CLR
+		// WINRT_RoUninitialize();
+	}
+
+}
+
+
---
+++ NativeApp/MainPage.h
@@ -0,0 +1,28 @@
+﻿//
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::NativeApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+		void MyOtherButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& e);
+	};
+}
+
+namespace winrt::NativeApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
